b586194a5b47c9ff3e67bc20c227e186c9ac48b7
Diogo <65966727+diogopsa@users.noreply.github.com>
2022-08-02 23:52:12 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,8 +4,15 @@ print("Bem vindo ao jogo!")
 print("*************************************")
 numero_secreto = 42
 chute = int(input("Digite o seu chute: "))
-if (numero_secreto == chute):
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+
+if (acertou):
     print("Parabéns!! Você acertou!")
 else:
+    if (maior):
+        print("O número que você chutou é maior que o número secreto!!")
+    else:
+        print("O número que você chutou é menor que o número secreto!!")
     print("Que pena, não foi dessa vez! Você errou!")
 print("Fim do jogo")
