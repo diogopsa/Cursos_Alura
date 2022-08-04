9a746295a660f10998191fbeae4b2ce7cf727466
Diogo <65966727+diogopsa@users.noreply.github.com>
2022-08-04 14:37:08 -0300

    Jogo da forca
    Jogo do tipo forca, exemplo que trabalha com listas e strings.

---
+++ forca.py
@@ -0,0 +1,13 @@
+def jogar():    
+    print("*************************************")
+    print("Bem vindo ao jogo!")
+    print("*************************************")
+    palavra_chave = "banana"
+    enforcou = False
+    acertou = False
+    while (not enforcou and not acertou):
+        print("Jogando...")
+        
+    print("Fim do jogo")
+if (__name__ == "__main__"):
+    jogar()
