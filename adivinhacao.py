1b12b9e8ccbff0871c4b36bc67b7bf23f9192eb3
Diogo <65966727+diogopsa@users.noreply.github.com>
2022-08-04 10:34:21 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,21 +1,37 @@
+import random
 print("*************************************")
 print("Bem vindo ao jogo!")
 print("*************************************")
-numero_secreto = 42
-rodada = 1
-total_de_tentativas = 3
-while rodada <= total_de_tentativas:
-    print("Tentativa {} de {}".format(rodada,total_de_tentativas))
+numero_secreto = random.randrange(1,101)
+total_de_tentativas = 0
+pontos = 1000
+print("Qual o nivel de dificuldade?")
+print("[1] facil, [2] medio ou [3] dificil?")
+nivel = int(input("Digite o nivel de dificuldade: "))
+if (nivel ==1):
+    total_de_tentativas = 20
+elif (nivel ==2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+for rodada in range (1,total_de_tentativas+1):
+    print("Rodada {} de {}".format(rodada, total_de_tentativas))
     chute = int(input("Digite o seu chute: "))
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
+    if (chute < 1 or chute > 100):
+        print("Voce deve entrar com um valor entre 1 e 100!!")
+        continue
+    print("Tentativa {} de {}".format(rodada,total_de_tentativas))
+    
     if (acertou):
-        print("Parabens!! Voce acertou!")
+        print("Parabens!! Voce acertou e fez {}!".format(pontos))
+        break
     else:
         if (maior):
             print("O numero que voce chutou foi maior que o numero secreto!!")
         else:
             print("O numero que voce chutou foi menor que o numero secreto!!")
-        print("Que pena, nao foi dessa vez! Voce errou!")
-    rodada = rodada +1
+            pontos_perdidos = abs(chute - numero_secreto)
+            pontos = (pontos - pontos_perdidos)
 print("Fim do jogo")
