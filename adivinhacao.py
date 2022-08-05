95d5eb090892be359c522b29a4f1a0ec101ff281
Diogo <65966727+diogopsa@users.noreply.github.com>
2022-08-05 16:27:59 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -9,9 +9,9 @@ def jogar():
     print("Qual o nivel de dificuldade?")
     print("[1] facil, [2] medio ou [3] dificil?")
     nivel = int(input("Digite o nivel de dificuldade: "))
-    if (nivel ==1):
+    if (nivel == 1):
         total_de_tentativas = 20
-    elif (nivel ==2):
+    elif (nivel == 2):
         total_de_tentativas = 10
     else:
         total_de_tentativas = 5
