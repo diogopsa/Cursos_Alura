0275defa178ce6eb6b5259f8b81028af4ae6fa84
Diogo <65966727+diogopsa@users.noreply.github.com>
2022-08-09 13:38:58 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -2,10 +2,19 @@ def jogar():
     print("*************************************")
     print("Bem vindo ao jogo!")
     print("*************************************")
+    
     palavra_chave = "banana"
+    
     enforcou = False
     acertou = False
+    
     while (not enforcou and not acertou):
+        chute = input("Qual a letra?")
+        index = 0
+        for letra in palavra_chave:
+            if chute == letra:                
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+                index = index + 1
         print("Jogando...")
         
     print("Fim do jogo")
