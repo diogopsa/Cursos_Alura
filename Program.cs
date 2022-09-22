46f8dcc03bc3f8039726842f228b8c82b5b73440
Diogo <65966727+diogopsa@users.noreply.github.com>
2022-09-21 22:01:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CriandoVariaveis
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            Console.WriteLine("Executando projeto 2 - criando variáveis");
+            int idade;
+            idade = 32;
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+
+    }
+
+}
